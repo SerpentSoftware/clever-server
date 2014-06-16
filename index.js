03d104d913e6583cc1e73a61db74f0eb9dab5ee7
Mike Lyons <mdl0394@gmail.com>
2014-06-15 17:42:35 -0700

    Finished styling of main window

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 var JB = require( "jabber-server" );
 
-var main_user = new JB.User( "main@clever", "Main User" );
+function startServer() {
+    var main_user = new JB.User( "main@clever", "Main User" );
 
-JB.Server.prototype.initUserList = function( ) {
-    var android_user = new JB.User( "android@clever", "Android User" );
-    var kocsy = new JB.User( "kocsy@clever", "Kocsy" );
+    JB.Server.prototype.initUserList = function( ) {
+        var android_user = new JB.User( "android@clever", "Android User" );
+        var kocsy = new JB.User( "kocsy@clever", "Kocsy" );
 
-    main_user.addBuddy( android_user );
-    main_user.addBuddy( kocsy );
+        main_user.addBuddy( android_user );
+        main_user.addBuddy( kocsy );
 
-    android_user.addBuddy( main_user );
-    android_user.addBuddy( kocsy );
+        android_user.addBuddy( main_user );
+        android_user.addBuddy( kocsy );
 
-    kocsy.addBuddy( android_user );
-    kocsy.addBuddy( main_user );
-    return [ main_user, android_user, kocsy ];
+        kocsy.addBuddy( android_user );
+        kocsy.addBuddy( main_user );
+        return [ main_user, android_user, kocsy ];
+    }
+
+    var server = new JB.Server({ domain: "10.0.0.16", port: 5223 });
+
+    server.on( 'registration-success', function( user ) {
+        console.log( "registration" + user.getKey() );
+
+        main_user.addBuddy( user );
+    } );
 }
-
-var server = new JB.Server({ domain: "10.0.0.16", port: 5223 });
-
-server.on( 'registration-success', function( user ) {
-    console.log( "registration" + user.getKey() );
-
-    main_user.addBuddy( user );
-} );
 
 
 var win = require('nw.gui').Window.get();
 win.setResizable( false );
 
 window.onload = function() {
+    win.showDevTools();
     initMenuBar();
+
+    startServer();
 }

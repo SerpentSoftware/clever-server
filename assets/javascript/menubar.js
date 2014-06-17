67ff2431a6225b72423e60ab80e1af7a4f0ac07e
Mike Lyons <mdl0394@gmail.com>
2014-06-17 08:17:14 -0700

    Added ui interactivity. Allows setting of main user

--- assets/javascript/menubar.js
+++ assets/javascript/menubar.js
@@ -1,10 +1,10 @@
 function setupCloseButton() {
     var button = document.getElementById( "close" )
     button.onmouseover = function() {
-        button.src = "assets/images/close_hover.png";
+        button.src = "assets/image/close_hover.png";
     }
     button.onmouseout = function() {
-        button.src = "assets/images/close.png";
+        button.src = "assets/image/close.png";
     }
     button.onclick = function() {
         var win = gui.Window.get();
--- index.js
+++ index.js
@@ -1,38 +1,56 @@
-var JB = require( "jabber-server" );
-
-function startServer() {
-    var main_user = new JB.User( "main@clever", "Main User" );
-
-    JB.Server.prototype.initUserList = function( ) {
-        var android_user = new JB.User( "android@clever", "Android User" );
-        var kocsy = new JB.User( "kocsy@clever", "Kocsy" );
-
-        main_user.addBuddy( android_user );
-        main_user.addBuddy( kocsy );
-
-        android_user.addBuddy( main_user );
-        android_user.addBuddy( kocsy );
-
-        kocsy.addBuddy( android_user );
-        kocsy.addBuddy( main_user );
-        return [ main_user, android_user, kocsy ];
-    }
-
-    var server = new JB.Server({ domain: "10.0.0.16", port: 5223 });
-
-    server.on( 'registration-success', function( user ) {
-        console.log( "registration" + user.getKey() );
-
-        main_user.addBuddy( user );
-    } );
-}
-
+var server = require( "./lib/server" )
 
 var win = require('nw.gui').Window.get();
 win.setResizable( false );
 
-window.onload = function() {
-    initMenuBar();
+function startServer( username, password ) {
+    var JB = server.startServer( username, password );
 
-    startServer();
+    JB.on( 'online', function() {
+        var server_ip = document.getElementById( "server_ip" );
+        server_ip.innerHTML = server.ip;
+
+        var server_ip = document.getElementById( "server_port" );
+        server_ip.innerHTML = server.port;
+    } );
+}
+
+window.onload = function() {
+    var username = null;
+    var password = null;
+    var main_account_username_field = document.getElementById( "account_username" );
+    var main_account_password_field = document.getElementById( "account_password" );
+
+    // TODO: Should try to load username/password from settings file
+
+    if( username != null && password != null ) {
+        main_account_username_field = username;
+        main_account_password_field = "michael9";
+        main_account_username_field.disabled = true;
+        main_account_password_field.disabled = true;
+        startServer( username, password );
+    }
+
+    var main_account_button = document.getElementById( "account_submit" );
+    main_account_button.onclick = function() {
+        if( main_account_username_field.value != "" && main_account_username_field.value != null &&
+            main_account_password_field.value != "" && main_account_password_field.value != null ) {
+            username = main_account_username_field.value;
+
+            if( !(new String(username)).indexOf( "@" ) != -1 ) {
+                username = username + "@clever";
+                main_account_username_field.value = username;
+            }
+
+            password = main_account_password_field.value;
+
+            startServer( username, password );
+
+            main_account_username_field.disabled = true;
+            main_account_password_field.disabled = true;
+            main_account_button.innerHTML = "Edit Account";
+        }
+    };
+
+    initMenuBar();
 }
